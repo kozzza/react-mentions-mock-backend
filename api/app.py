edccb8e9941bafa432a9760059bf6976431478e6
koza <kozy.boy13@gmail.com>
2023-01-01 18:19:29 -0800

    Changed CORS origins

--- api/app.py
+++ api/app.py
@@ -27,7 +27,8 @@ origins = [
     "http://localhost",
     "http://localhost:3000",
     "http://localhost:3000",
-    
+    "http://react-mentions-mock.vercel.app",
+    "https://react-mentions-mock.vercel.app"
 ]
 
 app.add_middleware(
